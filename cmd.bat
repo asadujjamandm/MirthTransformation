a245e24fcb5ee73ad7d79ce803214ec758488885
asad shohan <vdr_asohan@parata.com>
2022-04-05 16:54:27 +0000

    modifying command

--- cmd.bat
+++ cmd.bat
@@ -1,5 +1,2 @@
-#!/bin/bash
-dir = "/home/centusadmin/mirth/MirthTransformation/commands.txt"
-
-mccommand -a https://localhost:8443 -u asad_shohan -p ABcd12!@ -s $dir 
+mccommand -a https://localhost:8443 -u asad_shohan -p ABcd12!@ -s "/home/centusadmin/mirth/MirthTransformation/commands.txt" 
 
