366e75df7ab6d2d780dd7a37991235e37fd4c8c7
asad shohan <vdr_asohan@parata.com>
2022-04-12 09:08:13 +0000

    adding new file

--- cmd.bat
+++ cmd.bat
@@ -1,1 +1,1 @@
-mccommand -a https://localhost:8443 -u asad_shohan -p ABcd12!@ -s "/home/centusadmin/mirth/MirthTransformation/commands.txt" 
+mccommand -a https://localhost:8443 -u admin -p admin -s "/home/centusadmin/mirth/MirthTransformation/commands.txt" 
